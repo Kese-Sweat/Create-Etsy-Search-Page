28cc149818fb5190e191d0aca8e988e17ab40a3c
Markese Straughter <Kese08@hotmail.com>
2020-02-29 14:57:23 -0800

    started-layout

---
+++ main.js
@@ -0,0 +1,34 @@
+const first = items.results[0]
+console.log('first item', first)
+const gridItems = items.results.map((item) => {
+    return {
+        title: item.title,
+        image: item.Images[0].url_fullxfull
+    }
+})
+
+const gridList = gridItems.map((item) => {
+    return `
+        <div class="card">
+            <div>
+                <div class="card-image" style="background-image: url(${item.image})"></div>
+                <h2>${ item.title }</h2>
+            </div>
+        </div>
+        
+    `
+}).join('')
+
+document.querySelector('#grid').innerHTML = gridList;
+
+
+const topMenuBar = ['Jewelry & Accessories', 'Clothing & Shoes', 'Home & Living', 'Wedding & Party', 'Toys & Entertainment', 'Art & Collectibles', 'Craft Supplies', 'Vintage, Gifts']
+
+const categoryBar = topMenuBar.map(item =>{
+    return`
+        <li class="item">${item}</li>`
+        
+
+}).join('')
+
+document.querySelector('#menuBarOne').innerHTML= categoryBar
