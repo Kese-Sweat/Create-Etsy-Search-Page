6001207de2e509f4907b20df00cb15b2d685804e
Markese Straughter <Kese08@hotmail.com>
2020-03-04 18:18:58 -0800

    addedWordsToColumn

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const first = items.results[0]
 console.log('first item', first)
-const gridItems = items.results.map((item) => {
+const gridItems = items.results.map((item)=>{
     return {
         title: item.title,
         image: item.Images[0].url_fullxfull,
         shopName: item.Shop.shop_name,
         review: 4.0,
+        price: item.price,
+        
     }
 })
+
+const freeShipping = items.results.filter((item)=>{
+    return item.title.includes('FREE SHIPPING')
+})
+    
+
+
 
 const gridList = gridItems.map((item) => {
     return `
@@ -22,6 +31,8 @@ const gridList = gridItems.map((item) => {
                 <div>
                 <div class="Stars" style="--rating: ${item.review};" aria-label="Rating of this product is 4.0 out of 5."></div>
                 </div>
+                <span>${item.price}</span>
+                
             </div>
         </div>
         
@@ -41,5 +52,9 @@ const categoryBar = topMenuBar.map(item =>{
 }).join('')
 
 document.querySelector('#menuBarOne').innerHTML= categoryBar
+
+
+
+
 
 
